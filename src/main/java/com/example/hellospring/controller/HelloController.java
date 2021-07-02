7274e7128dbf3549b4c4f525ac5b1c9ec27f6757
junyeon1997 <junyeon2012@gmail.com>
2021-07-03 01:42:57 +0900

    api 방식으로 정보 전송

--- src/main/java/com/example/hellospring/controller/HelloController.java
+++ src/main/java/com/example/hellospring/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -14,9 +15,35 @@ public class HelloController {
         return "hello";
     }
 
-    @GetMapping("hello-mvc")
+    @GetMapping("hello-mvc") //템플릿 엔진으로 조작
     public String helloMvc(@RequestParam("name") String name, Model model) {
         model.addAttribute("name",name);
         return "hello-template";
+    }
+
+    @GetMapping("hello-spring")
+    @ResponseBody //HTTP의 BODY에 문자내용을 직접 내려줌
+    public String helloString(@RequestParam("name") String name) {
+        return "hello" + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody //api방식이면 json으로 반환
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
