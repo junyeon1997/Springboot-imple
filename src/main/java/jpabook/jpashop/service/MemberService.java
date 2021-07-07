8cbcd75a4943807c064d5294cf253f6bf88156df
junyeon1997 <junyeon2012@gmail.com>
2021-07-07 14:44:30 +0900

    Chore: 롬복으로 코드 간단하게 수정

--- src/main/java/jpabook/jpashop/service/MemberService.java
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -2,6 +2,7 @@ package jpabook.jpashop.service;
 
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -10,14 +11,12 @@ import java.util.List;
 
 @Service
 @Transactional(readOnly = true) // 읽기 전용 최적화
+@RequiredArgsConstructor
 public class MemberService {
 
 
     private final MemberRepository memberRepository;
 
-    public MemberService(MemberRepository memberRepository) {
-        this.memberRepository = memberRepository;
-    }
 
     /**
      * 회원 가입
