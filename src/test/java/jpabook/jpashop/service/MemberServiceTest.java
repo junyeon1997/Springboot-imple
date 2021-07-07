5df30ccb487b656e8e2d8457bbae4173cd885875
junyeon1997 <junyeon2012@gmail.com>
2021-07-07 15:52:55 +0900

    Chore: 회원 서비스 기능 테스트 케이스 수정

--- src/test/java/jpabook/jpashop/service/MemberServiceTest.java
+++ src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -6,7 +6,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -34,7 +33,7 @@ public class MemberServiceTest {
 
     }
 
-    @Test
+    @Test(expected = IllegalStateException.class)
     public void 중복_회원_예외() throws Exception {
         //given
         Member member1 = new Member();
@@ -45,11 +44,7 @@ public class MemberServiceTest {
 
         //when
         memberService.join(member1);
-        try {
-            memberService.join(member2); //예외가 발생해야 한다.
-        } catch (IllegalStateException e) {
-            return;
-        }
+        memberService.join(member2); //예외가 발생해야 한다.
 
         //then
         fail("예외가 발생해햐 한다");
