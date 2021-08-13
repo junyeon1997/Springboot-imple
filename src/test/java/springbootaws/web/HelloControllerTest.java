8640bc308d9c5650c17854041318252fb4fa3b6c
junyeon1997 <junyeon2012@gmail.com>
2021-08-13 16:56:35 +0900

    [Test] 테스트 코드 수정(스프링 시큐리티 적용)

--- src/test/java/springbootaws/web/HelloControllerTest.java
+++ src/test/java/springbootaws/web/HelloControllerTest.java
@@ -1,11 +1,18 @@
 package springbootaws.web;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultMatcher;
+import springbootaws.config.auth.SecurityConfig;
 
 import static net.bytebuddy.matcher.ElementMatchers.is;
 import static org.junit.jupiter.api.Assertions.*;
@@ -13,14 +20,19 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@SpringBootTest
-@AutoConfigureMockMvc
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(controllers = HelloController.class,
+        excludeFilters = {
+                @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)
+        }
+)
 class HelloControllerTest {
 
     @Autowired
     private MockMvc mvc;
 
     @Test
+    @WithMockUser(roles="USER")
     public void helloTest() throws Exception{
         String hello = "hello";
 
@@ -29,6 +41,7 @@ class HelloControllerTest {
                 .andExpect(content().string(hello));
     }
     @Test
+    @WithMockUser(roles="USER")
     public void helloDtoTest() throws Exception {
         //given
         String name = "hello";
